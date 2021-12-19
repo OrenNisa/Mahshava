8b6ec205cc0dcaa1cade09ac697cba450eb6c018
galpartuk <76867700+galpartuk@users.noreply.github.com>
2021-12-19 20:17:44 +0200

    Added the sidebar logo's components

---
+++ code/client/mahshava/src/components/sidebar/logo/HistorySchoolLogo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HistoryLogo from '../../Substract.svd';
+
+const HistorySchoolLogo = () => {
+    return (
+        <div>
+            <img src={HistoryLogo} alt={'history-school-logo'} width={150} height={50} />
+        </div>
+    );
+};
+
+
+export default HistorySchoolLogo;
