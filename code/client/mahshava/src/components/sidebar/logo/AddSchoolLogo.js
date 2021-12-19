50a683be7473c7f548e6b6d6a6589124d2c135a9
galpartuk <76867700+galpartuk@users.noreply.github.com>
2021-12-19 20:17:44 +0200

    Added the sidebar logo's components

---
+++ code/client/mahshava/src/components/sidebar/logo/AddSchoolLogo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddLogo from '../../svg.svd';
+
+const AddSchoolLogo = () => {
+    return (
+        <div>
+            <img src={AddLogo} alt={'add-school-logo'} width={150} height={50} />
+        </div>
+    );
+};
+
+
+export default AddSchoolLogo;
---
+++ code/client/mahshava/src/components/sidebar/logo/MainPageLogo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLogo from '../../Vector.svd';
+
+const MainPageLogo = () => {
+    return (
+        <div>
+            <img src={MainLogo} alt={'main-page-logo'} width={150} height={50} />
+        </div>
+    );
+};
+
+
+export default MainPageLogo;
