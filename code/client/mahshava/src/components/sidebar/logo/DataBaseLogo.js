3e0c4f1f29ceca323d0313bc05956fdb47f6a158
galpartuk <76867700+galpartuk@users.noreply.github.com>
2021-12-19 20:17:44 +0200

    Added the sidebar logo's components

---
+++ code/client/mahshava/src/components/sidebar/logo/DataBaseLogo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DbLogo from '../../Vector_13.svd';
+
+const DataBaseLogo = () => {
+    return (
+        <div>
+            <img src={DbLogo} alt={'data-base-logo'} width={150} height={50} />
+        </div>
+    );
+};
+
+
+export default DataBaseLogo;
