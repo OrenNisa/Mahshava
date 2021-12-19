a38cffdb0d39932ba33423842e1fd4bc8a88b44b
Michael Chen <76773254+mcWAFFLEZ@users.noreply.github.com>
2021-12-19 22:12:46 +0200

    Final changes before merge

--- code/client/mahshava/src/components/header/Header.js
+++ code/client/mahshava/src/components/header/Header.js
@@ -15,9 +15,8 @@ const Header = () => {
             <NotificationsIcon/>
             <MailIcon/>
             <CalendarIcon/>
-            <svg className={'line'}><svg width="2" height="49" viewBox="0 0 2 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className={'line'} width="2" height="49" viewBox="0 0 2 49" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <line x1="0.951172" y1="49" x2="0.951174" y2="-2.18557e-08" stroke="#2F3A5F"/>
-            </svg>
             </svg>
             <UserIcon/>
             <div className="logoutButton">
