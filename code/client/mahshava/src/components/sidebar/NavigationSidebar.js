dfbe50119dd8101a2f6a75b8c36a48f9139d6ac4
Or Arbiv <Arbivor100@gmail.com>
2021-12-20 15:52:43 +0200

    used prettier and fixed PlaceHolderLogo.js name

--- code/client/mahshava/src/components/sidebar/NavigationSidebar.js
+++ code/client/mahshava/src/components/sidebar/NavigationSidebar.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import style from './NavigationSidebar.module.css';
-import PlaceHolderLogo from "./placeHolderLogo";
+import PlaceHolderLogo from "./PlaceHolderLogo";
 
 
 const NavigationSidebar = () => {
-    return(
-  <div className={style.sidebarContainer}>
-    <div className={style.logo}>
-        <PlaceHolderLogo/>
-    </div>
-      <div className={style.logo}>
-          <PlaceHolderLogo/>
-      </div>
-      <div className={style.logo}>
-          <PlaceHolderLogo/>
-      </div>
-      <div className={style.logo}>
-          <PlaceHolderLogo/>
-      </div>
-      <div className={style.bottomLogo}>
-          <PlaceHolderLogo/>
-      </div>
-  </div>
+    return (
+        <div className={style.sidebarContainer}>
+            <div className={style.logo}>
+                <PlaceHolderLogo/>
+            </div>
+            <div className={style.logo}>
+                <PlaceHolderLogo/>
+            </div>
+            <div className={style.logo}>
+                <PlaceHolderLogo/>
+            </div>
+            <div className={style.logo}>
+                <PlaceHolderLogo/>
+            </div>
+            <div className={style.bottomLogo}>
+                <PlaceHolderLogo/>
+            </div>
+        </div>
     );
 
 };
--- code/client/mahshava/src/components/sidebar/PlaceHolderLogo.js
+++ code/client/mahshava/src/components/sidebar/PlaceHolderLogo.js
@@ -4,9 +4,9 @@ import HistoryLogo from './logo/Substract.svg';
 const PlaceHolderLogo = () => {
     return (
         <div>
-            <img src={HistoryLogo} alt={'main-page-logo'} width={150} height={50} />
+            <img src={HistoryLogo} alt={'main-page-logo'} width={150} height={50}/>
         </div>
     );
 };
 
-export default  PlaceHolderLogo;
+export default PlaceHolderLogo;
