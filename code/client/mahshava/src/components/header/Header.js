02c0fa969ea2ac7e452dd06b0ec7517adfb41b1b
galpartuk <76867700+galpartuk@users.noreply.github.com>
2021-12-20 16:31:13 +0200

    intergrated header's logo into the header provided Substract.svg as sidebar's placeholder icon

--- code/client/mahshava/src/components/header/Header.js
+++ code/client/mahshava/src/components/header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Header.css';
 import Logout from '../auth/Logout';
-
+import CalendarIcon from "./icons/CalendarIcon";
+import NotificationsIcon from "./icons/NotificationsIcon";
+import MailIcon from "./icons/MailIcon";
+import UserIcon from "./icons/UserIcon";
 const Header = () => {
     return (
         <div className="header-container">
-            <div className="logo1">logo1</div>
-            <div className="logo2">logo2</div>
-            <div className="logo3">logo3</div>
-            <div className="logo4">logo4</div>
+            <div className="NotificationsIcon"><NotificationsIcon/></div>
+            <div className="MailIcon"><MailIcon/></div>
+            <div className="CalendarIcon"><CalendarIcon/></div>
+            <div className="UserIcon"><UserIcon/></div>
             <div className="userGreeting">hello guest</div>
             <div className="logoutButton">
                 <Logout />
