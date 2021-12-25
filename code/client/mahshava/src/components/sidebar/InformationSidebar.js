2a37d702a0794e80b6719a24f51cb5cd70beb34c
ジャイ子 <76917679+JenyaIvanov@users.noreply.github.com>
2021-12-25 20:11:47 +0200

    - Now prettier was set correctly.

--- code/client/mahshava/src/components/sidebar/InformationSidebar.js
+++ code/client/mahshava/src/components/sidebar/InformationSidebar.js
@@ -3,24 +3,24 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import style from './InformationSidebar.module.css';
-import {Avatar} from "@mui/material";
-import {useIntl} from "react-intl";
+import { Avatar } from '@mui/material';
+import { useIntl } from 'react-intl';
 
 const InformationSidebar = () => {
     const { formatMessage } = useIntl();
     return (
         <Card className={style.SchoolInformation}>
             <CardContent>
-
                 <div className={style.SchoolsInProgress}>
-                    <Avatar className={style.SchoolCounter} > 6 </Avatar>
+                    <Avatar className={style.SchoolCounter}> 6 </Avatar>
                     {formatMessage({ id: 'SchoolInformationSideBar.SchoolsInProgress.message' })}
                 </div>
 
                 <div className={style.ResilienceCheck}>
                     <div className={style.SchoolProgressHeader}>
-                        ^  {formatMessage({ id: 'SchoolInformationSideBar.ResilienceCheck.header' })}
-                        <span className={style.HeaderAmount}>4</span><span className={style.HeaderTotal}>/6</span>
+                        ^ {formatMessage({ id: 'SchoolInformationSideBar.ResilienceCheck.header' })}
+                        <span className={style.HeaderAmount}>4</span>
+                        <span className={style.HeaderTotal}>/6</span>
                     </div>
 
                     <Typography variant="body2" color="text.secondary">
@@ -40,8 +40,9 @@ const InformationSidebar = () => {
 
                 <div className={style.InterventionCheck}>
                     <div className={style.SchoolProgressHeader}>
-                        ^  {formatMessage({ id: 'SchoolInformationSideBar.InterventionCheck.header' })}
-                        <span className={style.HeaderAmount}>2</span><span className={style.HeaderTotal}>/6</span>
+                        ^ {formatMessage({ id: 'SchoolInformationSideBar.InterventionCheck.header' })}
+                        <span className={style.HeaderAmount}>2</span>
+                        <span className={style.HeaderTotal}>/6</span>
                     </div>
 
                     <Typography variant="body2" color="text.secondary">
@@ -58,18 +59,13 @@ const InformationSidebar = () => {
 
                 <div className={style.InterventionCheck}>
                     <div className={style.SchoolProgressHeader}>
-                        V  {formatMessage({ id: 'SchoolInformationSideBar.ReevaluationCheck.header' })}
-                        <span className={style.HeaderAmount}>0</span><span className={style.HeaderTotal}>/6</span>
+                        V {formatMessage({ id: 'SchoolInformationSideBar.ReevaluationCheck.header' })}
+                        <span className={style.HeaderAmount}>0</span>
+                        <span className={style.HeaderTotal}>/6</span>
                     </div>
-
-                    <Typography variant="body2" color="text.secondary">
-                        <list>
-
-                        </list>
-                    </Typography>
                 </div>
             </CardContent>
         </Card>
     );
-}
+};
 export default InformationSidebar;
