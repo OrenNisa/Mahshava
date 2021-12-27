a9a88eaa93493693ef61c739c2083bacfcb46ddb
ジャイ子 <76917679+JenyaIvanov@users.noreply.github.com>
2021-12-27 18:50:34 +0200

    - Header now presents the name & lastname of the connected user.

--- code/client/mahshava/src/components/header/Header.js
+++ code/client/mahshava/src/components/header/Header.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import style from './Header.module.css';
 import Logout from '../auth/Logout';
-import CalendarIcon from "./icons/CalendarIcon";
-import NotificationsIcon from "./icons/NotificationsIcon";
-import MailIcon from "./icons/MailIcon";
-import UserIcon from "./icons/UserIcon";
+import CalendarIcon from './icons/CalendarIcon';
+import NotificationsIcon from './icons/NotificationsIcon';
+import MailIcon from './icons/MailIcon';
+import UserIcon from './icons/UserIcon';
+import HeaderUser from './HeaderUser';
 const Header = () => {
     return (
         <div className={style.headerContainer}>
-            <div className={style.NotificationsIcon}><NotificationsIcon/></div>
-            <div className={style.MailIcon}><MailIcon/></div>
-            <div className={style.CalendarIcon}><CalendarIcon/></div>
-            <div className={style.UserIcon}><UserIcon/></div>
-            <div className={style.userGreeting}>hello guest</div>
+            <div className={style.NotificationsIcon}>
+                <NotificationsIcon />
+            </div>
+            <div className={style.MailIcon}>
+                <MailIcon />
+            </div>
+            <div className={style.CalendarIcon}>
+                <CalendarIcon />
+            </div>
+            <div className={style.UserIcon}>
+                <UserIcon />
+            </div>
+            <div className={style.userGreeting}>
+                <HeaderUser />
+            </div>
             <div className={style.logoutButton}>
                 <Logout />
             </div>
