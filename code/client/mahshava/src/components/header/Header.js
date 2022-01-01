ddc1e3f99449563eb03b43b00f964f536ffae81d
galpartuk <76867700+galpartuk@users.noreply.github.com>
2022-01-01 16:32:25 +0200

    fixed UI to look like the figma:
    fixed the table to be smaller and fit into the content layout
    
    fixed the sidebar last icon to fit into the bottom, also fixed sidebar to be across the entire page and intergrated the new logos
    
    added HelpLogo.js which was missing from the logo folder
    
    added line between user icon and calender icon
    removed Substract.svg and PlaceHolderLogo.js since both are no longer in usage

--- code/client/mahshava/src/components/header/Header.js
+++ code/client/mahshava/src/components/header/Header.js
@@ -6,13 +6,14 @@ import NotificationsIcon from "./icons/NotificationsIcon";
 import MailIcon from "./icons/MailIcon";
 import UserIcon from "./icons/UserIcon";
 import LineIcon from "./icons/LineIcon"
+
 const Header = () => {
     return (
         <div className={style.headerContainer}>
             <div className={style.NotificationsIcon}><NotificationsIcon/></div>
             <div className={style.MailIcon}><MailIcon/></div>
             <div className={style.CalendarIcon}><CalendarIcon/></div>
-            <div className={style.LineIcon}> <LineIcon /> </div>
+            <div className={style.LineIcon}><LineIcon/></div>
             <div className={style.UserIcon}><UserIcon/></div>
             <div className={style.userGreeting}>hello guest</div>
         </div>
--- code/client/mahshava/src/components/header/icons/LineIcon.js
+++ code/client/mahshava/src/components/header/icons/LineIcon.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const LineIcon = () => {
     return (
-        <svg className={"line-icon"} width="2" height="49" viewBox="0 0 2 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className={"line-icon"} width="2" height="49" viewBox="0 0 2 49" fill="none"
+             xmlns="http://www.w3.org/2000/svg">
             <line x1="0.951172" y1="49" x2="0.951174" y2="-2.18557e-08" stroke="#2F3A5F"/>
         </svg>
 
