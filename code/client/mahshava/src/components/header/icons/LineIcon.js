14ad746cc9c1c8b5a9572102256f5ec90c748da5
galpartuk <76867700+galpartuk@users.noreply.github.com>
2022-01-01 20:01:32 +0200

    fixed UI to look like the figma:
    fixed the table to be smaller and fit into the content layout
    
    fixed the sidebar last icon to fit into the bottom, also fixed sidebar to be across the entire page and intergrated the new logos
    
    added HelpLogo.js which was missing from the logo folder
    
    added line between user icon and calender icon
    removed Substract.svg and PlaceHolderLogo.js since both are no longer in usage

---
+++ code/client/mahshava/src/components/header/icons/LineIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LineIcon = () => {
+    return (
+        <svg className={"line-icon"} width="2" height="49" viewBox="0 0 2 49" fill="none"
+             xmlns="http://www.w3.org/2000/svg">
+            <line x1="0.951172" y1="49" x2="0.951174" y2="-2.18557e-08" stroke="#2F3A5F"/>
+        </svg>
+
+    );
+};
+
+export default LineIcon;
