7b80738c4813044bcc7fd1e4a1c5b57b40ea2011
galpartuk <76867700+galpartuk@users.noreply.github.com>
2022-01-01 16:30:37 +0200

    fixed UI to look like the figma:
    fixed the table to be smaller and fit into the content layout
    
    fixed the sidebar last icon to fit into the bottom, also fixed sidebar to be across the entire page and intergrated the new logos
    
    added HelpLogo.js which was missing from the logo folder
    
    added line between user icon and calender icon
    removed Substract.svg and PlaceHolderLogo.js since both are no longer in usage

--- code/client/mahshava/src/components/header/Header.js
+++ code/client/mahshava/src/components/header/Header.js
@@ -5,17 +5,16 @@ import CalendarIcon from "./icons/CalendarIcon";
 import NotificationsIcon from "./icons/NotificationsIcon";
 import MailIcon from "./icons/MailIcon";
 import UserIcon from "./icons/UserIcon";
+import LineIcon from "./icons/LineIcon"
 const Header = () => {
     return (
         <div className={style.headerContainer}>
             <div className={style.NotificationsIcon}><NotificationsIcon/></div>
             <div className={style.MailIcon}><MailIcon/></div>
             <div className={style.CalendarIcon}><CalendarIcon/></div>
+            <div className={style.LineIcon}> <LineIcon /> </div>
             <div className={style.UserIcon}><UserIcon/></div>
             <div className={style.userGreeting}>hello guest</div>
-            <div className={style.logoutButton}>
-                <Logout />
-            </div>
         </div>
     );
 };
