371eaa75ba62056f1db3a060a9f7b5db9ee8e48c
OmriMashiah <omrimashiahofficial@gmail.com>
2022-03-19 17:59:23 +0200

    Added 2 buttons to the home page: first button redirects to survey creator. second button is empty currently.

--- code/client/surveys/src/App.js
+++ code/client/surveys/src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
 import Creator from './components/Creator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-    return <Creator />;
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <input type='button' value='Survey Creator' onClick={this.renderCreator} />
+                <input type='button' value='Survey List' onClick={this.renderSurveyList} />
+            </>
+        );
+    }
+
+    renderCreator() {
+
+        ReactDOM.render(<Creator/>, document.getElementById('root'));
+    }
+
+    renderSurveyList() {
+        ReactDOM.render(<p>Testing</p>, document.getElementById('root'));
+    }
 }
 
 export default App;
---
+++ code/client/surveys/src/components/Home.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import Creator from "./Creator";
+//
+// // This is a functional component
+// const Home=()=>
+// {
+//
+//     const MoveToCreator=()=> {
+//         console.log("Hello World!")
+//         return <Creator />;
+//     }
+//
+//     return (
+//         <>
+//             <h1>Hello, welcome to Surveys Project :)</h1>
+//             <button onClick={MoveToCreator}>Survey Creator</button>
+//             <button>Display Creator</button>
+//         </>
+//     );
+// }
+//
+// export default Home;
+//
+//
