46a841b0de31e36514343655493c0a95a512ec6e
DESKTOP-RQII8SU\Oren Nisanov <orennisa@gmail.com>
2022-03-24 00:01:39 +0200

    more backend editing

--- code/server/src/mahshava/core/serializers.py
+++ code/server/src/mahshava/core/serializers.py
@@ -1,17 +1,35 @@
 from rest_framework import serializers
 
-from .models import School, Process
-
-
-class ProcessSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Process
-        fields = "__all__"
+from .models import School, Contact, Task, ProcessSteps, SchoolProcess
 
 
 class SchoolSerializers(serializers.ModelSerializer):
-    process = ProcessSerializers(read_only=True, many=True)
+    # process = ProcessSerializers(read_only=True, many=True)
 
     class Meta:
         model = School
+        fields = "__all__"
+
+
+class ContactSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = "__all__"
+
+
+class TaskSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+
+class ProcessStepsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ProcessSteps
+        fields = "__all__"
+
+
+class SchoolProcessSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = SchoolProcess
         fields = "__all__"
