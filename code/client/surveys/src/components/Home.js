16746e653b738c1966a5f9dd77dfa620215f3ed0
Eliyahu Yakobov <yugoej@gmail.com>
2022-03-28 19:11:45 +0300

    Fixed visuals

--- code/client/surveys/src/components/Home.js
+++ code/client/surveys/src/components/Home.js
@@ -3,33 +3,35 @@ import { useNavigate } from 'react-router-dom';
 
 function Home() {
 
-    // const buttonStyle = {
-//     position: 'relative',
-//     marginLeft: '20px',
-//     left: '38.5%',
-//     top: '150px',
-//     width: '150px',
-//     height: '150px',
-//     fontSize: '18px',
-//     backgroundColor: 'lightblue',
-//     borderRadius: '100px',
-// }
-//
-// const titleStyle = {
-//     position: 'relative',
-//     textAlign: 'center',
-//     color: 'darkblue',
-//     top: '40px',
-//     fontFamily: 'Helvetica',
-//     fontSize: '75px',
-// }
+    const buttonStyle = {
+    position: 'relative',
+    marginLeft: '20px',
+    left: '38.5%',
+    top: '150px',
+    width: '150px',
+    height: '150px',
+    fontSize: '18px',
+    backgroundColor: 'lightblue',
+    borderRadius: '100px',
+}
+
+const titleStyle = {
+    position: 'relative',
+    textAlign: 'center',
+    color: 'darkblue',
+    top: '40px',
+    fontFamily: 'Helvetica',
+    fontSize: '75px',
+}
 
     let navigate = useNavigate();
 
     return (
         <>
-        <input type='button' value='Survey Creator' onClick={() => {navigate("/Creator")}}></input>
-        <input type='button' value='Survey List' onClick={() => {navigate("/SurveyList")}}></input>
+        <h1 style={titleStyle}>Surveys</h1>
+
+        <input type='button' style={buttonStyle} value='Survey Creator' onClick={() => {navigate("/Creator")}}></input>
+        <input type='button' style={buttonStyle} value='Survey List' onClick={() => {navigate("/SurveyList")}}></input>
         </>
 );
 }
