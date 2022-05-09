089d7cab86b5edba327e6a49d3eb6eeab0c6cbba
OmriMashiah <omrimashiahofficial@gmail.com>
2022-05-09 18:08:02 +0300

    Redesigned Homepage (Home.js)

--- code/client/surveys/src/components/Home.js
+++ code/client/surveys/src/components/Home.js
@@ -1,36 +1,63 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 
+
 function Home() {
 
-    const buttonStyle = {
-    position: 'relative',
-    marginLeft: '20px',
-    left: '38.5%',
-    top: '150px',
-    width: '150px',
-    height: '150px',
-    fontSize: '18px',
-    backgroundColor: 'lightblue',
-    borderRadius: '100px',
-}
+    const creatorButtonStyle = {
 
-const titleStyle = {
-    position: 'relative',
-    textAlign: 'center',
-    color: 'darkblue',
-    top: '40px',
-    fontFamily: 'Helvetica',
-    fontSize: '75px',
-}
+        position: 'relative',
+        marginLeft: '20px',
+        left: '15%',
+        bottom: '140px',
+        width: '200px',
+        height: '80px',
+        fontSize: '25px',
+        fontWeight: 'bold',
+        //border: 'solid 0px black',
+        //backgroundColor: 'lightblue',
+        borderRadius: '10px',
+    }
+
+    const listButtonStyle = {
+
+        position: 'relative',
+        marginTop: '30px',
+        left: '20%',
+        fontWeight: 'bold',
+        bottom: '140px',
+        width: '200px',
+        height: '80px',
+        fontSize: '25px',
+
+        borderRadius: '10px',
+    }
+
+    const homeBackground ={
+
+        backgroundImage: 'url("https://cdn.discordapp.com/attachments/896788926193365033/970715609220391032/pexels-gradienta-7130468.jpg")',
+        width: '100%',
+        height: '100%',
+        marginTop: '10%',
+    }
+
+    const logoStyle ={
+
+        marginTop: '50px',
+    }
 
     let navigate = useNavigate();
 
     return (
         <>
-        <h1 style={titleStyle}>Surveys</h1>
-        <input type='button' style={buttonStyle} value='Survey Creator' onClick={() => {navigate("/Creator")}}></input>
-        <input type='button' style={buttonStyle} value='Survey List' onClick={() => {navigate("/SurveyList")}}></input>
+        <div style={homeBackground}>
+
+            <img style={logoStyle} src={"https://cdn.discordapp.com/attachments/896788926193365033/970713087881654392/survey-system_logo.png"}/>
+
+            <input type='button' style={creatorButtonStyle} value='CREATE' onClick={() => {navigate("/Creator")}}></input>
+            <input type='button' style={listButtonStyle} value='LIST' onClick={() => {navigate("/SurveyList")}}></input>
+        </div>
+
         </>
 );
 }
