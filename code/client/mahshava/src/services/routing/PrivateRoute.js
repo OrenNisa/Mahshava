97a69953a556289a13ccb2e523553d3f4db17b11
DESKTOP-RQII8SU\Oren Nisanov <orennisa@gmail.com>
2022-05-11 19:41:09 +0300

    fix routing problem

--- code/client/mahshava/src/services/routing/PrivateRoute.js
+++ code/client/mahshava/src/services/routing/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router';
-import { Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router';
 import useAuth from '../auth/hooks/useAuth';
 import { LOGIN_ROUTE } from './routes';
 
