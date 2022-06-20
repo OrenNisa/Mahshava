f9b7b04250bb82227c3357e980eff0baf287ea78
DESKTOP-RQII8SU\Oren Nisanov <orennisa@gmail.com>
2022-06-20 23:39:32 +0300

    add and fix filtering and sorting option on main table.

--- code/client/mahshava/src/components/mahshava-admin/AutoLayout/HeaderAutoLayout.js
+++ code/client/mahshava/src/components/mahshava-admin/AutoLayout/HeaderAutoLayout.js
@@ -2,8 +2,6 @@ import React from 'react';
 import style from './HeaderAutoLayout.module.css';
 import Layout from './Layout';
 import NameAllSchools from './NameAllSchools';
-import SearchLine from './SearchLine';
-import Filtering from "./Filtering";
 import MainName from "./MainName";
 
 
@@ -18,12 +16,6 @@ const HeaderAutoLayout = () => {
                 </div>
                 <div className={style.StrSchool}>
                     <NameAllSchools />
-                </div>
-                <div className={style.FilteringLog}>
-                    <Filtering />
-                </div>
-                <div className={style.Search}>
-                    <SearchLine />
                 </div>
             </div>
     );
