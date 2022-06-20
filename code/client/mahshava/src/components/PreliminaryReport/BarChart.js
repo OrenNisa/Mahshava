914d919339bc67d8f5b7f6c5f930cf9c51ca4ca6
DESKTOP-RQII8SU\Oren Nisanov <orennisa@gmail.com>
2022-06-21 00:19:02 +0300

    all charts added to initial report

--- code/client/mahshava/src/components/PreliminaryReport/BarChart.js
+++ code/client/mahshava/src/components/PreliminaryReport/BarChart.js
@@ -3,7 +3,37 @@ import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS } from "chart.js/auto";
 
 function BarChart({ chartData }) {
-    return <Bar data={chartData} />;
+    const options = {
+        responsive: true,
+        scales: {
+            x: {
+                ticks: {
+                    font: {
+                        size: 7
+                    }
+                },
+                grid: {
+                    display: false
+                }
+            },
+            y: {
+                ticks: {
+                    max: 7,
+                    suggestedMax: 7,
+                },
+                grid: {
+                    display: true
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    };
+
+    return <Bar data={chartData} options={options}/>;
 }
 
 export default BarChart;
--- code/client/mahshava/src/components/PreliminaryReport/RadarChart.js
+++ code/client/mahshava/src/components/PreliminaryReport/RadarChart.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
+import {Radar} from "react-chartjs-2";
+import BarChart from "./BarChart";
 
-function RadarChart() {
-    return (
-        <div></div>
-    );
+function RadarChart({chartData}) {
+    const options = {
+        responsive: true,
+        scales: {
+            r: {
+                display: true,
+                ticks: {
+                    display: false,
+                },
+                grid: {
+                    circular: false,
+                },
+                pointLabels: {
+                    display: false,
+                }
+            }
+        },
+        elements: {
+            arc:{
+                borderWidth : 0
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    };
+
+    return <Radar data={chartData} options={options}/>;
 }
 
 export default RadarChart;
